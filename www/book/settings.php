d23de4cf69be323992f741d666b46bfe20664768
Kevin Hammer <kevin@hammerlabs.com>
2013-11-24 13:13:27 -0800

    set readme and settings.php

--- www/book/settings.php
+++ www/book/settings.php
@@ -29,35 +29,17 @@ switch (ENVIRONMENT) {
 	case 'development':
 		define("CDN", "../../cdn/book/");
 		define("BLOGNAME", "ahdev.tumblr.com");
-		define("CONSUMER_KEY", "CTz2LZ01VUTVhdoib2XM9wDvdE5bphn9wmsi3zyTmYrtmTuMhD");
-		define("CONSUMER_SECRET", "hod74WSG3ZLRJs2tdOO0FWRuxt4gRRyxnzJbj2auC9E4FD5iI0");
-		define("OAUTH_TOKEN", "ri7IoyC2uNo56yRdXE4qgzAMepQPdaHt28FLEBXYu6kSSb2ixv");
-		define("OAUTH_SECRET", "LARTp5dptZN6X6wO2D8bXPMyishfz4nExEm1x3znmIfwjsP1gx"); 
-		define("TWITTER_CONSUMER_KEY", "b5SgSCBxWyhao6NzykdrQ"); 
-		define("TWITTER_CONSUMER_SECRET", "PjzQQxPrEazIlk3AyUfpxZ1AvFbbhVIkiS4pETfJU"); 
 		break;
 
 	case 'testing':
 		define("CDN", "http://stage.sonypictures.com/origin-flash/movies/americanhustle/tumblr/book/");
 		define("BLOGNAME", "americanhustlemoviedev.tumblr.com"); 
-		define("CONSUMER_KEY", "VTrFOn4QnQvbFZ8T99yzbA1uEbITTvjZOxHhdBJ6b5sZvTElwe");
-		define("CONSUMER_SECRET", "dRtvkuC0CrNNFelkomO7h1kiNwHQBW0BhzNWUQ4j7QFRt0v9Qw");
-		define("OAUTH_TOKEN", "ojLMdByU0IyDKCaMZHyq84Tifg8DxoMQzaYnfvlKsizqu0GSZs");
-		define("OAUTH_SECRET", "EYBF7qfIfzbEgbj1Aw3Dq2XosPha4YWGfDh9ktqBJt6SO0exti"); 
-		define("TWITTER_CONSUMER_KEY", "b5SgSCBxWyhao6NzykdrQ"); 
-		define("TWITTER_CONSUMER_SECRET", "PjzQQxPrEazIlk3AyUfpxZ1AvFbbhVIkiS4pETfJU"); 
 		break;
 
 	case 'production':
 		error_reporting(0);
 		define("CDN", "http://flash.sonypictures.com/movies/americanhustle/tumblr/book/");    
 		define("BLOGNAME", "americanhustlemovie.tumblr.com");            
-		define("CONSUMER_KEY", "");
-		define("CONSUMER_SECRET", "");
-		define("OAUTH_TOKEN", "");
-		define("OAUTH_SECRET", ""); 
-		define("TWITTER_CONSUMER_KEY", "b5SgSCBxWyhao6NzykdrQ"); 
-		define("TWITTER_CONSUMER_SECRET", "PjzQQxPrEazIlk3AyUfpxZ1AvFbbhVIkiS4pETfJU"); 
 		break;
 
 	default:
@@ -65,54 +47,4 @@ switch (ENVIRONMENT) {
 }
 
 $share_blogname = BLOGNAME; 
-//$preload = createImagePreload("assets/img");
 $webroot = curPagePath(); 
-
-
-
-
-
-
-function setEnvironment($list) {
-	$_host_name = $_SERVER[ 'HTTP_HOST' ];
-	$_this_env = 'development'; // this is the default env
-	foreach ( $list as $env_name => $env_urls ) {
-		foreach ( $env_urls as $url ) {
-			if ( preg_match( "/{$url}$/", $_host_name ) ) {
-				$_this_env = $env_name; // boom, we found it
-				break 2;
-			}
-		}
-	}
-	define( 'ENVIRONMENT', $_this_env );
-}
-
-function curPagePath() {
-	$protocol = strtolower(array_shift(explode("/", $_SERVER["SERVER_PROTOCOL"])));
-	$pathArr = explode("/", $_SERVER["SCRIPT_NAME"]);
-	array_pop($pathArr);
-	$path = implode("/",$pathArr)."/";
-	if ($_SERVER["SERVER_PORT"] != "80") {
-		$fullpath = $protocol."://".$_SERVER["HTTP_HOST"].":".$_SERVER["SERVER_PORT"].$path;
-	} else {
-		$fullpath = $protocol."://".$_SERVER["HTTP_HOST"].$path;
-	}
-	return $fullpath;
-}
-
-function createImagePreload($dir) { 
-    $root = scandir($dir); 
-    foreach($root as $value) { 
-        if($value === '.' || $value === '..') {continue;} 
-        if(is_file("$dir/$value")) {$result[]="$dir/$value";continue;} 
-        foreach(createImagePreload("$dir/$value") as $value) { 
-            $result[]=$value; 
-        } 
-    } 
-	$preload = "\t\t<div style='display: none;''>\n";
-	foreach ($result as $img){
-		$preload .= "\t\t\t<img src='".CDN.str_replace("assets/", "", $img)."' />\n";
-	}
-	$preload .= "\t\t</div>";
-	return $preload;
-} 
